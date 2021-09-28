ca3af0451b234b2bd42e0cbd9bce3b288dc62343
DeinsOne <b.velichko2004@yandex.ru>
2021-09-28 15:36:57 +0300

    Envi function documentation

--- README.md
+++ README.md
@@ -1,22 +1,37 @@
 # EnvInteraction
-Cross platform c++ library for interaction with window environment. Window capturing and event sending in real time
+Cross platform c++ library for interaction with window environment. There is built-in real time window capturing and event sending
 
 ## Goal
-Goal of this project is to build easy emendable cross platform library to interact with graphical interface from c++.
+Goal of this project is to build easy embeddable cross platform library for interacting with graphical interface from c++
+
+Currently built and tested with Linux and Windows
 
  - [x] Window capturing
-    - [x] Linux window capturing
-    - [x] Windows window capturing
  - [x] Recovering images to disk
     - [x] Programmable limitation
     - [x] Multithreading
- - [ ] Inputs
-    - [ ] Mouse buttons
-    - [ ] Mouse movement
-    - [ ] Keyboard events
+ - [x] Inputs
+    - [x] Mouse events
+    - [x] Keyboard events
 
 Potentially this project will be used in computer vision one to interact with real time frame changing and immediately reacting to this
 
 ## Requirement
 
+- Linux: Xinerama Xtst Xfixes
+- Windows: dwmapi
+
 ## Build
+
+There is premake extension file for premake5 build system. Just call premake.exe with specified target and compile generated project. More details are [there](https://premake.github.io/docs/) 
+
+```
+Ubuntu 
+$> sudo apt-get install libxinerama-dev libxtst-dev libxcb-xfixes0 
+$> premake5 gmake
+$> make config=release
+
+Windows
+$> premake5.exe vs2019
+$> ../MSBuild.exe Environment.sln
+```
--- envi/include/Envi.h
+++ envi/include/Envi.h
@@ -10,9 +10,7 @@
 #include <thread>
 #include <vector>
 
-/**
- * Environment interaction
-*/
+
 namespace Envi {
 
     struct Image;
@@ -52,7 +50,14 @@ namespace Envi {
         float Scaling = 1.0f;
     };
 
+    /**
+     * @return Returns list of opened windows
+    */
     ENVI_EXTERN std::vector<Window> GetWindows();
+
+    /**
+     * @return Returns list of connected monitors
+    */
     ENVI_EXTERN std::vector<Monitor> GetMonitors();
 
     ENVI_EXTERN int Height(const Image &img);
@@ -60,16 +65,28 @@ namespace Envi {
     ENVI_EXTERN const ImageBGRA* StartSrc(const Image &img);
     ENVI_EXTERN const ImageBGRA* GotoNextRow(const Image &img, const ImageBGRA *current);
 
-    // Callbecks
+    // WindowCaptureCallback
     typedef std::function<void(const Envi::Image &img, const Window &window)> WindowCaptureCallback;
+    // WindowChangeCallback
     typedef std::function<void(const Window &window)> WindowChangeCallback;
+    // WindowCallback is used to define window to be captured
     typedef std::function<std::vector<Window>()> WindowCallback;
 
     class ICapturerManager {
         public:
-            // Control flow
+            /**
+             * Stop window capturing
+            */
             virtual void Pause() = 0;
+
+            /**
+             * @return Check if capturing is paused
+            */
             virtual bool IsPaused() = 0;
+
+            /**
+             * Resume capturing if it was stopped
+            */
             virtual void Resume() = 0;
 
     };
@@ -77,14 +94,33 @@ namespace Envi {
     template <typename T, typename C>
     class ICaptureConfiguration {
         public:
+            /**
+             * Set up callbeck for every tick
+             * @param cb [WindowCaptureCallback] callback
+            */
             virtual std::shared_ptr<ICaptureConfiguration<T, C>> OnNewFrame(const T &cb) = 0;
 
+            /**
+             * Set up callbeck for window resizing
+             * @param cb [WindowChangeCallback] callback
+            */
             virtual std::shared_ptr<ICaptureConfiguration<T, C>> OnFrameChanged(const C &cb) = 0;
 
+            /**
+             * Set up tick time. Analogous of FPS
+             * @param milliseconds time restriction for every tick
+            */
             virtual std::shared_ptr<ICaptureConfiguration<T, C>> SetTickInterval(int milliseconds) = 0;
 
+            /**
+             * Set up image recovering mode. If is enabled every frame from tick will be saved ion disk
+             * @param recover Conditional variable
+            */
             virtual std::shared_ptr<ICaptureConfiguration<T, C>> SetRecoverImages(bool recover = true) = 0;
 
+            /**
+             * Factory to start window capturing and to get contorl flow interface
+            */
             virtual std::shared_ptr<ICapturerManager> startCapturing() = 0;
     };
 
@@ -150,10 +186,15 @@ namespace Envi {
     template <typename Event, typename Duration>
     ENVI_EXTERN void SendInput(const Event &e, const Duration& time);
 
+    // KeyCallback
     typedef std::function<void(const KeyEvent& cb)> KeyCallback;
+    // MouseButtonCallback
     typedef std::function<void(const MouseButtonEvent& cb)> MouseButtonCallback;
+    // MouseScrollCallback
     typedef std::function<void(const MouseScrollEvent& cb)> MouseScrollCallback;
+    // MousePositionOffsetCallback
     typedef std::function<void(const MousePositionOffsetEvent& cb)> MousePositionOffsetCallback;
+    // MousePositionAbsoluteCallback
     typedef std::function<void(const MousePositionAbsoluteEvent& cb)> MousePositionAbsoluteCallback;
 
     class ENVI_EXTERN IInputManager {
@@ -161,29 +202,99 @@ namespace Envi {
             using Ms = std::chrono::milliseconds;
 
         public:
+            /**
+             * Add KeyEvent to queue
+             * @param e Event
+             * @param time event duration
+            */
             virtual void PushEvent(const KeyEvent &e, const Ms time = Ms(0)) = 0;
+
+            /**
+             * Add MouseButtonEvent to queue
+             * @param e Event
+             * @param time event duration
+            */
             virtual void PushEvent(const MouseButtonEvent &e, const Ms time = Ms(0)) = 0;
+
+            /**
+             * Add MouseScrollEvent to queue
+             * @param e Event
+             * @param time event duration
+            */
             virtual void PushEvent(const MouseScrollEvent &pos, const Ms time = Ms(0)) = 0;
+
+            /**
+             * Add MousePositionOffsetEvent to queue
+             * @param e Event
+             * @param time event duration
+            */
             virtual void PushEvent(const MousePositionOffsetEvent &pos, const Ms time = Ms(0)) = 0;
+
+            /**
+             * Add MousePositionAbsoluteEvent to queue
+             * @param e Event
+             * @param time event duration
+            */
             virtual void PushEvent(const MousePositionAbsoluteEvent &pos, const Ms time = Ms(0)) = 0;
 
+            /**
+             * Stop executing event queue
+            */
             virtual void Pause() = 0;
+
+            /**
+             * Check if event queue is stopped
+            */
             virtual bool IsPaused() = 0;
+
+            /**
+             * Continue executing event queue
+            */
             virtual void Resume() = 0;
 
+            /**
+             * Clear event queue
+            */
             virtual void Flush() = 0;
+
+            /**
+             * Wait until event queue became empty
+            */
             virtual void Wait() = 0;
 
     };
 
     class ENVI_EXTERN IInputConfiguration {
         public:
+            /**
+             * Add a callback to be called every time a KeyEvent occurs
+            */
             virtual std::shared_ptr<IInputConfiguration> OnEvent(const KeyCallback& cb) = 0;
+
+            /**
+             * Add a callback to be called every time a MouseButtonCallback occurs
+            */
             virtual std::shared_ptr<IInputConfiguration> OnEvent(const MouseButtonCallback& cb) = 0;
+
+            /**
+             * Add a callback to be called every time a MouseScrollCallback occurs
+            */
             virtual std::shared_ptr<IInputConfiguration> OnEvent(const MouseScrollCallback& cb) = 0;
+
+            /**
+             * Add a callback to be called every time a MousePositionOffsetCallback occurs
+            */
             virtual std::shared_ptr<IInputConfiguration> OnEvent(const MousePositionOffsetCallback& cb) = 0;
+
+            /**
+             * Add a callback to be called every time a MousePositionAbsoluteCallback occurs
+            */
             virtual std::shared_ptr<IInputConfiguration> OnEvent(const MousePositionAbsoluteCallback& cb) = 0;
 
+            /**
+             * Start executing event queue
+             * @return Input manager to allow flow control
+            */
             virtual std::shared_ptr<IInputManager> startListening() = 0;
     };
 
